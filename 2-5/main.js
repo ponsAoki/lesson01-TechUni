1a2629b524c8b0361636796d3b08022adba8a624
ponsAoki <aokishogo@gmail.com>
2022-06-04 15:38:04 +0900

    2-5

--- 2-5/main.js
+++ 2-5/main.js
@@ -8,33 +8,32 @@ const result = program.args;
 // console.log(result);
 
 let number = parseInt(result[0])
-    // console.log(String(number).length);
-    // let numLen = String(number).length
-    // console.log(number);
-    // const min = 0
-    // let a = Math.random() + min
-    // let b = Math.random() + min
-    // let c = Math.random() + min
+
 const arr = []
-for (let a = 0; 100 * a <= number; a++) {
-    for (let b = 0; 10 * b <= number; b++) {
-        for (let c = 0; 1 * c <= number; c++) {
-            if ((100 * a + 10 * b + 1 * c) == number && a > b && b > c) {
-                const sum = a + b + c;
-                arr.push({sum: sum, a: a, b: b, c: c})
-                // const MinSum = Math.min(sum)
-                    // const min = Math.min(a)
-                // console.log(arr);
+const sumArr = []
+const AllCal = () => {
+        try {
+            for (let a = 0; 100 * a <= number; a++) {
+                for (let b = 0; 10 * b <= number; b++) {
+                    for (let c = 0; 1 * c <= number; c++) {
+                        if ((100 * a + 10 * b + 1 * c) == number) {
+                            const sum = a + b + c
+                            arr.push({ sum: sum, a: a, b: b, c: c })
+                            sumArr.push(sum)
+                        }
+                    }
+                }
             }
-
+        } catch (error) {
+            console.error(error);
         }
-
     }
-}
-const minSum = arr[arr.length - 1].sum
-const A = arr[arr.length - 1].a
-const B = arr[arr.length - 1].b
-const C = arr[arr.length - 1].a
-// const minArr = Math.min(...arr[sum])
-console.log(minSum);
-// console.log(Math.min(...arr));
+    // console.log(sumArr);
+AllCal();
+const minSum = Math.min(...sumArr)
+    // console.log(minSum);
+arr.forEach(object => {
+    if (object.sum === minSum) {
+        console.log("100円玉" + object.a + "枚、" + "10円玉" + object.b + "枚、" + "1円玉" + object.c + "枚");
+    }
+})
